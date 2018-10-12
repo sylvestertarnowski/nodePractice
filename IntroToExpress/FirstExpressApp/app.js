3da2b37cc2cbe07be9b32f9a5f13a187f5340e1d
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-12 10:21:03 +0000

    adding routes to express app

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -15,6 +15,23 @@ app.get("/dog", function(req, res) {
     res.send("MEOW!");
 });
 
+app.get("/r/:subredditName", function(req, res) {
+    console.log(req.params);
+    res.send("Welcome to a subreddit");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    var subreddit = req.params.subredditName;
+    res.send("Welcome to the " + subreddit.toUpperCase() + ", baby!");
+});
+
+// Tell Express to display the below every time
+// get request is unrecognised 
+// should always be at the bottom!!!
+app.get("*", function(req, res) {
+    res.send("YOU ARE A STAR!!!");
+});
+
 // Tell Express to listen for requests (start server)
 
 app.listen(process.env.PORT, process.env.IP, function() {
