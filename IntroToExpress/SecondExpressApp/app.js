4deeb790cd75bb893aaf5d9ebd53b45c65bffee3
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-15 16:43:15 +0000

    Finished assignment in Exxpress

--- IntroToExpress/SecondExpressApp/app.js
+++ IntroToExpress/SecondExpressApp/app.js
@@ -28,13 +28,19 @@ app.get("/speak/:id", function(req, res) {
 });
 
 app.get("/repeat/:word/:number", function(req, res) {
+    console.log("word and number repetition accessed");
     var word = req.params.word;
-    var times = req.params.number;
+    var times = Number(req.params.number);
     var finalSentence = "";
     for(var i = 0; i < times; i++) {
         finalSentence += (word + " ");
     }
     res.send(finalSentence);
+});
+
+app.get("*", function(req, res) {
+    console.log("random url accessed");
+    res.send("Sorry, page not found...What are you doing with your life?");
 });
 
 //Tell express to listen for requests
