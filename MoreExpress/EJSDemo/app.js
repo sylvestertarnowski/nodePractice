e3b85d66d7fea0c1aedf87f6123dc6bddbb1aca3
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-21 15:00:19 +0000

    EJS dynamic page implemented

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -7,7 +7,7 @@ app.get("/", function(req, res) {
 
 app.get("/fallinlovewith/:thing", function(req, res) {
     var thing = req.params.thing;
-    res.send("You fell in love with " + thing);
+    res.render("love.ejs", {thingVar: thing});
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
