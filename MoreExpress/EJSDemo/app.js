3efe80a00f0287afa88177345e47b1181906216c
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-23 04:19:53 +0000

    added partials and css

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
 
+//tell express how to access public folder
+app.use(express.static("public"));
+//tell express that all files are ejs
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res) {
@@ -17,7 +22,7 @@ app.get("/posts", function(req, res) {
         {title: "Amazing supplements", author: "Tusia"},
     ];
     
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
