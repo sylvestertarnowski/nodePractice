4ec14465f605a29a7d82ed0acfb19ceec7b69650
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-25 04:56:27 +0000

    final version omdb app

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -3,8 +3,15 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
-    request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+    var query = req.query.search;
+    var url = "http://omdbapi.com/?s=";
+    var apikey = "&apikey=thewdb";
+    request(url + query + apikey, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             res.render("results", {data: data});
