9e42080112a4e80abf634a5ce449603e19f0fef6
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-25 04:42:31 +0000

    OMDB API APP

---
+++ APIs/movie_search_app/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.get("/results", function(req, res){
+    request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
+        }
+    });
+});
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Movie App has started!!!");
+});
