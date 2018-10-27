3eefc05a65dc94d1c56c4874ac45846def84cc7c
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-27 05:53:23 +0000

    minor fixes

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,19 +1,34 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://images.freeimages.com/images/large-previews/1bc/camping-1407985.jpg"},
+    {name: "Bear Grounds", image: "https://images.freeimages.com/images/large-previews/b22/camping-tent-2-1427666.jpg"},
+    {name: "St Mountain", image: "https://images.freeimages.com/images/large-previews/19a/tent-1-1552981.jpg"}
+];
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://images.freeimages.com/images/large-previews/1bc/camping-1407985.jpg"},
-        {name: "Bear Grounds", image: "https://images.freeimages.com/images/large-previews/b22/camping-tent-2-1427666.jpg"},
-        {name: "St Mountain", image: "https://images.freeimages.com/images/large-previews/19a/tent-1-1552981.jpg"}
-    ];
-    
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
