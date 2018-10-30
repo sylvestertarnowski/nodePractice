3aaa8818eb4d92a66b1a49be8e1c8d5ce19b126d
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-30 06:28:19 +0000

    First attempts at interactive blog

---
+++ RESTful/RESTfulBlogApp/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//MONGOOSE MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.new}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1442605527737-ed62b867591f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ca2e3968074340e0d2593cb4852383b5&auto=format&fit=crop&w=1050&q=80",
+//     body: "Hello this is a blog post"
+// });
+
+//RESTFUL ROUTES
+
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("error!");
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is running");
+});
