bcd8e0cc8a6e6fd35ea7fc085af67c619f104ec5
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-30 05:26:52 +0000

    Communication with mongodb server changed

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,7 +10,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -18,7 +19,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //                     {
 //                         name: "Bear Grounds", 
-//                         image: "https://images.freeimages.com/images/large-previews/b22/camping-tent-2-1427666.jpg"
+//                         image: "https://images.freeimages.com/images/large-previews/b22/camping-tent-2-1427666.jpg",
+//                         description: "This is a huge granite hill, no bathrooms. Now Water. Beatiful granite!"
                         
 //                     }, function(err, campground){
 //                       if(err){
@@ -53,16 +55,17 @@ app.get("/campgrounds", function(req, res){
         if(err){
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
-    });
+    }); 
     // res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description; 
+    var newCampground = {name: name, image: image, description: desc};
     //create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
@@ -75,6 +78,17 @@ app.post("/campgrounds", function(req, res){
 
 app.get("/campgrounds/new", function(req, res){
     res.render("new.ejs");
+});
+
+//The SHOW page following Restful convention
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("show", {campground: foundCampground});
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
