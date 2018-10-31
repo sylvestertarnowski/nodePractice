d09a9a7cbdcd2f691422ea823a3a165b770f133b
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-10-31 06:05:29 +0000

    New Blog page added

--- RESTful/RESTfulBlogApp/app.js
+++ RESTful/RESTfulBlogApp/app.js
@@ -40,6 +40,19 @@ app.get("/blogs", function(req, res){
     });
 });
 
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/blogs", function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+});
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
