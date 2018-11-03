0aff15732460b8b77f4e3caf5ee00fcb432c5277
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-03 15:45:24 +0000

    Show page for blog

--- RESTful/RESTfulBlogApp/app.js
+++ RESTful/RESTfulBlogApp/app.js
@@ -51,9 +51,20 @@ app.post("/blogs", function(req, res){
         } else {
             res.redirect("/blogs");
         }
-    })
+    });
 });
 
+//SHOW ROUTE
+
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
