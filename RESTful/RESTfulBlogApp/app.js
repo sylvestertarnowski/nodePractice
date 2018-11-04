255cdc7d78419459af576c6043c3ae64a3ad12ca
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-04 08:55:57 +0000

    Edit, Delete, Update added

--- RESTful/RESTfulBlogApp/app.js
+++ RESTful/RESTfulBlogApp/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //MONGOOSE MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -79,7 +81,13 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
-    res.send("Update route");
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
 });
 
 // DELETE ROUTE
