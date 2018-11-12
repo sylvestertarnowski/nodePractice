362caae99e58b922cdb32ce1e03aa9e650ec1683
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-12 14:38:50 +0000

    login added

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -140,7 +140,8 @@ app.get("/login", function(req, res){
     res.render("login");
 });
 
-app.post("/login", passport.authenticate("local", {
+app.post("/login", passport.authenticate("local", 
+{
     successRedirect: "/campgrounds",
     failureRedirect: "/login"
 }), function(req, res){
