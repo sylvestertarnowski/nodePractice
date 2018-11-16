f66274bbf2c8875562dd7eb5437ed6d8944c8247
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-16 06:59:56 +0000

    Landing Page finished, price feature added

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -21,13 +21,14 @@ router.get("/", function(req, res) {
 //create new campground
 router.post("/", middleware.isLoggedIn, function(req, res) {
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = { name: name, image: image, description: desc, author: author };
+    var newCampground = { name: name, price: price, image: image, description: desc, author: author };
     //create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
